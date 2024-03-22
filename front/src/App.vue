7401858169cacc84543dc8974e87c844fbc75f8e
nathan <cornelie.nathanpro@gmail.com>
2024-03-22 11:37:50 +0100

    [+] start home page

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,17 +1,17 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import NavBar from "./components/NavBar.vue";
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <v-app>
+    <NavBar />
+    <div class="content">
+
+    </div>
+    <router-view/>
+
+  </v-app>
+
 </template>
 
 <style scoped>
@@ -26,5 +26,8 @@ import HelloWorld from './components/HelloWorld.vue'
 }
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
+}
+.content{
+  margin-top: 64px;
 }
 </style>
---
+++ front/src/components/Home/Stats.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+
+const title = "Statistics"
+
+</script>
+
+<template>
+<v-card :title></v-card>
+</template>
+
+<style scoped>
+
+</style>
